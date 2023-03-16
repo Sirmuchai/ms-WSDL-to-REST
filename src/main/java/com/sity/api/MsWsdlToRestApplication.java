b6a4815b488e19367a0c0a801977581bf7e9cb2d
Sirmuchai <sirmemu@gmail.com>
2023-03-16 10:40:27 +0300

    01. JABX2 plugin added

---
+++ src/main/java/com/sity/api/MsWsdlToRestApplication.java
@@ -0,0 +1,14 @@
+package com.sity.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWsdlToRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsWsdlToRestApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/sity/api/MsWsdlToRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sity.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsWsdlToRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
